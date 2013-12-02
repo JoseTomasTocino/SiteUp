a63b7af3406f80a5095e625feb4bf6c874c14b5c
José Tomás Tocino <theom3ga@gmail.com>
2013-12-02 22:59:37 +0100

    Added checks for http headers and content

--- experimental/monitoring.py
+++ experimental/monitoring.py
@@ -4,18 +4,19 @@
 import subprocess
 import re
 import dns.resolver
+import requests
 
-def check_ping(location):
-    """Sends a ping to the given location, and returns a dictionary with the results
+def check_ping(target):
+    """Sends a ping to the given target, and returns a dictionary with the results
 
-    :param location: the computer to ping, identified with either an IP or a domain name
+    :param target: the computer to ping, identified with either an IP or a domain name
 
     :returns whatever
     
     Returns a dictionary with the different fields"""
 
     # Launch ping process and get stdout's content
-    ping_raw_response = subprocess.Popen(["ping", "-c3", "-w10", location], stdout=subprocess.PIPE).stdout.read()
+    ping_raw_response = subprocess.Popen(["ping", "-c3", "-w10", target], stdout=subprocess.PIPE).stdout.read()
 
     # Compile regular expression
     matcher = re.compile(r"""
@@ -45,13 +46,13 @@ def check_ping(location):
         
     return results
 
-def check_dns(domain_name, register_type, expected_address):
+def check_dns(target, register_type, expected_address):
     """Checks if a certain domain has a DNS register (of the proper type) that
     matches the expected address"""
     
     try:
         # Launch the query
-        answer = dns.resolver.query(domain_name, register_type)
+        answer = dns.resolver.query(target, register_type)
 
     # On timeout, or any other problem, just return false
     except:
@@ -63,10 +64,25 @@ def check_dns(domain_name, register_type, expected_address):
             return True
 
     return False
-    
+
+def check_http_header(target, status_code):    
+    try:
+        r = requests.head(target)
+        return r.status_code == status_code
+    except:
+        return None
+
+def check_http_content(target, content_string):
+    try:
+        r = requests.get(target)
+        return content_string in r.text
+    except:
+        return None
 
 if __name__ == '__main__':
 #    check_ping('localhost')
-    print check_dns('josetomastocino.com', 'A', '78.47.140.228')
+#    print check_dns('josetomastocino.com', 'A', '78.47.140.228')
+    print check_http_header('http://josetomastocino.com/', 200)
+    print check_http_content('http://josetomastocino.com', 'Ingeniero')
 
 
