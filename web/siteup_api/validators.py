6fe7bc8966945f122f81636660971fb5dd6389b7
José Tomás Tocino <theom3ga@gmail.com>
2014-01-23 12:08:36 +0100

    Moved custom validators to separate file

---
+++ web/siteup_api/validators.py
@@ -0,0 +1,19 @@
+from django.core.exceptions import ValidationError
+
+class ValidateAnyOf(object):
+    """Receives a list of validators for a model field, and checks
+    if ANY of those validators passes. Raises ValidationError otherwise."""
+
+    def __init__(self, *validators):
+        self.validators = validators
+
+    def __call__(self, value):
+        errors = []
+        for validator in self.validators:
+            try:
+                validator(value)
+                return
+            except ValidationError as e:
+                errors.append(unicode(e.message) % e.params)
+
+        raise ValidationError('Combined validation failed: ' + ' '.join(errors))
