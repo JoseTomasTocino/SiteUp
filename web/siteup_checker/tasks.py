a56958614cd8e485b2bf43b2a2902f415f351940
José Tomás Tocino <theom3ga@gmail.com>
2014-02-18 01:36:50 +0100

    Testing celery capabilities, added a task 'triggerer'. Works well

--- web/siteup_checker/tasks.py
+++ web/siteup_checker/tasks.py
@@ -1,10 +1,29 @@
 from celery.task.schedules import crontab
 from celery.decorators import periodic_task
 
+from siteup.celery import app
 from siteup_api import models
+
+from time import sleep
+from random import randrange
+
+@app.task
+def nap(t):
+    sleep(t)
+    print "Slept for %i seconds" % t
+
 
 # this will run every minute, see http://celeryproject.org/docs/reference/celery.task.schedules.html#celery.task.schedules.crontab
 @periodic_task(run_every=crontab(hour="*", minute="*", day_of_week="*"))
 def test():
     kk = models.PingCheck.objects.all()
-    print "firing test task", kk
+    print "lol wut", kk
+    numtasks = randrange(2,6)
+    for f in range(numtasks):
+        nap.delay(randrange(5))
+
+    print "Enqueued %i tasks" % numtasks
+
+
+
+
