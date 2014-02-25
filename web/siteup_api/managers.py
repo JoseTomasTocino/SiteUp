fd1ef27fb2f9d2778ed6e12c952a180853496bf2
José Tomás Tocino <theom3ga@gmail.com>
2014-02-26 00:39:42 +0100

    Missing file from previous commit

---
+++ web/siteup_api/managers.py
@@ -0,0 +1,8 @@
+import datetime
+
+from django.db import models
+
+class CheckLogManager(models.Manager):
+
+    def latest_hours(self, *args, **kwargs):
+        return self.filter(date__gt=datetime.datetime.now() - datetime.timedelta(hours=12))
