b0e6d07714dd15171e847ee74dcbdd525843a862
José Tomás Tocino <theom3ga@gmail.com>
2014-02-25 15:16:44 +0100

    Fixed path for base dir in settings base file

--- web/siteup/settings/base.py
+++ web/siteup/settings/base.py
@@ -1,6 +1,6 @@
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), '..')
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(__file__)), '..'))
 
 
 # Quick-start development settings - unsuitable for production
