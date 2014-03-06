26a8858739e030b87503f2ed9825ec446729fb7d
José Tomás Tocino <theom3ga@gmail.com>
2014-03-06 18:04:26 +0100

    Moved db files to DB folder

--- web/siteup/settings/base.py
+++ web/siteup/settings/base.py
@@ -51,7 +51,7 @@ WSGI_APPLICATION = 'siteup.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db', 'django-db.sqlite3'),
     }
 }
 
