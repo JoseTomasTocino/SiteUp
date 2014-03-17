23c844bb12c39257070bda39184df468ff9561d1
José Tomás Tocino <theom3ga@gmail.com>
2014-03-17 23:21:41 +0100

    Added some summary figures in the detail page of the check2

--- web/siteup_api/managers.py
+++ web/siteup_api/managers.py
@@ -9,3 +9,26 @@ class CheckLogManager(models.Manager):
 
     def last_week(self, *args, **kwargs):
         return self.filter(date__gt=datetime.datetime.now() - datetime.timedelta(days=7), date__lt=datetime.datetime.now() - datetime.timedelta(hours=24))
+
+    @staticmethod
+    def _with_extra (q):
+        q_len = float(len(q))
+        resp_times = [int(x.response_time) for x in q]
+        avg_response_time = sum(resp_times) / q_len
+        avg_status = int(sum((int(x.status == 0) for x in q)) / q_len * 100)
+
+        return {
+            'objs' : q,
+            'avg_response_time': avg_response_time,
+            'avg_status': avg_status,
+            'max_response_time': max(resp_times),
+            'min_response_time': min(resp_times)
+        }
+
+    def last_24_hours_with_avg(self, *args, **kwargs):
+        q = self.filter(date__gt=datetime.datetime.now() - datetime.timedelta(hours=24))
+        return CheckLogManager._with_extra(q)
+
+    def last_week_with_avg(self, *args, **kwargs):
+        q = self.filter(date__gt=datetime.datetime.now() - datetime.timedelta(days=7), date__lt=datetime.datetime.now() - datetime.timedelta(hours=24))
+        return CheckLogManager._with_extra(q)
