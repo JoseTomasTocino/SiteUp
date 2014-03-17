eb97fbc1e7bb0ffccbe773c5a44821e67890e590
José Tomás Tocino <theom3ga@gmail.com>
2014-03-17 17:36:26 +0100

    Added last_week method to CheckLog manager to return logs from last week

--- web/siteup_api/managers.py
+++ web/siteup_api/managers.py
@@ -6,3 +6,6 @@ class CheckLogManager(models.Manager):
 
     def last_24_hours(self, *args, **kwargs):
         return self.filter(date__gt=datetime.datetime.now() - datetime.timedelta(hours=24))
+
+    def last_week(self, *args, **kwargs):
+        return self.filter(date__gt=datetime.datetime.now() - datetime.timedelta(days=7), date__lt=datetime.datetime.now() - datetime.timedelta(hours=24))
