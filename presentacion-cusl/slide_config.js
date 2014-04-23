e498f41e89f178901a6efd1591be349ccfb69560
José Tomás Tocino <theom3ga@gmail.com>
2014-04-23 02:26:57 +0200

    Added presentation for local phase of CUSL

---
+++ presentacion-cusl/slide_config.js
@@ -0,0 +1,40 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'SiteUp',
+    subtitle: 'Plataforma de monitorización de servicios de Internet',
+    eventInfo: {
+      title: 'Concurso Universitario de Software Libre &mdash; Fase local',
+      date: '23/04/2014'
+    },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold,bold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'José Tomás Tocino',
+    company: 'Universidad de Cádiz',
+    //gplus: 'http://plus.google.com/1234567890',
+    twitter: '@josetomastocino',
+    www: 'http://www.josetomastocino.com',
+    github: 'http://github.com/josetomastocino'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
